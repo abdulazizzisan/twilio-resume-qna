a3885066319b0252dce36eaf0fa794c672fbe549
abdulazizzisan <binabdulaziz4983@gmail.com>
2025-10-21 13:33:20 +0600

    initial commit

---
+++ src/main/java/dev/zisan/smsagent/SmsRecruiterSupportAgentApplication.java
@@ -0,0 +1,13 @@
+package dev.zisan.smsagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsRecruiterSupportAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsRecruiterSupportAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/zisan/smsagent/SmsRecruiterSupportAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.zisan.smsagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsRecruiterSupportAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
